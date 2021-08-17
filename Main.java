6f8bc9eb1da59c328e153ffdb0fd87b3fe7bfff1
DieuLam <s3877664@rmit.edu.vn>
2021-08-17 11:43:03 +0700

    fix object and add error handler

--- Main.java
+++ Main.java
@@ -4,32 +4,39 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException, ParseException {
-        Scanner input = new Scanner(System.in);
+        // create an empty object
+        Data date = new Data();
         // get country
+        Scanner input = new Scanner(System.in);
         System.out.print("Please choose a country: ");
         String country = input.nextLine();
-        // get date option
-        System.out.println("\n1: start date & end date \n2: number of days from a date \n3: number of weeks from a date");
-        System.out.print("Please choose a time range method (1/2/3): ");
-        int option = Integer.parseInt(input.nextLine());
-        // option 1
-        if (option == 1) {
-            dateOption.option_1(country); // call
-        // option 2
-        } else if (option == 2) {
-            dateOption.option_2(country); // call
-        // option 3
-        } else if (option == 3) {
-            dateOption.option_3(country); // call
+        if (errorHandler.checkCountry(date, country) == true) {
+            // get date option
+            System.out.println("\n1: start date & end date \n2: number of days from a date \n3: number of weeks from a date");
+            System.out.print("Please choose a time range method (1/2/3): ");
+            int option = Integer.parseInt(input.nextLine());
+            // option 1
+            if (option == 1) {
+                dateOption.option_1(date); // call
+            // option 2
+            } else if (option == 2) {
+                dateOption.option_2(date); // call
+            // option 3
+            } else if (option == 3) {
+                dateOption.option_3(date); // call
+            } else {
+                System.out.println("invalid option");
+            }
         } else {
-            System.out.println("invalid option");
+            System.out.println("\nWe can't find the country you are looking for");
         }
         input.close();
+        System.out.printf("\ncountry: %s, start: %s, end: %s", date.country, date.startDate, date.endDate);
     }
 }
 
 class dateOption {
-    public static void option_1(String country) throws IOException, ParseException {
+    public static Data option_1(Data date) throws IOException, ParseException {
         Scanner input = new Scanner(System.in);
         // get date
         System.out.print("\nStart date (MM/dd/yyyy): ");
@@ -37,90 +44,103 @@ class dateOption {
         System.out.print("End date (MM/dd/yyyy): ");
         String endDate = input.nextLine();
         input.close();
-        // calculate days range
-        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");   
-        Date start = formatter.parse(startDate);
-        Date end = formatter.parse(endDate); 
-        long days = ((end.getTime() - start.getTime()) / (1000*60*60*24)) % 365; // covert time to days
-        int totalDay = (int) days + 1; // convert long to int
-        // call
-        getData.getObject(country, startDate, totalDay);
+
+        // assign to object
+
+        date.startDate = startDate;
+        date.endDate = endDate;
+        return date;
     }
 
-    public static void option_2(String country) throws IOException, ParseException {
+    public static Data option_2(Data date) throws IOException, ParseException {
         Scanner input = new Scanner(System.in);
+
         // get date
         System.out.print("\nStart date (MM/dd/yyyy): ");
         String startDate = input.nextLine();
         System.out.printf("How many days from %s: ", startDate);
         int days = Integer.parseInt(input.nextLine());
-        input.close();
-        int totalDay = days + 1;   
-        // call
-        getData.getObject(country, startDate, totalDay);
+        input.close();   
+
+        // calculate end date;   
+        DateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+        Calendar c = Calendar.getInstance();
+        c.setTime(formatter.parse(startDate)); // convert from String to Date
+        c.add(Calendar.DATE, days); // add days
+        Date newDate = c.getTime(); 
+        String endDate = formatter.format(newDate); // covert back to string
+
+         // assign to object
+         date.startDate = startDate;
+         date.endDate = endDate;
+         return date;
     }
 
-    public static void option_3(String country) throws IOException, ParseException {
+    public static Data option_3(Data date) throws IOException, ParseException {
         Scanner input = new Scanner(System.in);
+
         // get date
         System.out.print("\nStart date (MM/dd/yyyy): ");
         String startDate = input.nextLine();
-        System.out.printf("How many week from %s: ", startDate);
+        System.out.printf("How many days from %s: ", startDate);
         int weeks = Integer.parseInt(input.nextLine());
-        input.close();
-        int totalDay = weeks * 7 + 1;   
-        // call
-        getData.getObject(country, startDate, totalDay);
+        input.close();   
+
+        // calculate end date; 
+        int days = weeks * 7;   
+        DateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+        Calendar c = Calendar.getInstance();
+        c.setTime(formatter.parse(startDate)); // convert from String to Date
+        c.add(Calendar.DATE, days); // add days
+        Date newDate = c.getTime();
+        String endDate = formatter.format(newDate);  // covert back to string
+;
+        // assign to object
+        date.startDate = startDate;
+        date.endDate = endDate;
+        return date;
     }
 }
 
-class getData {
-
-    public static Data[] getObject(String country, String startDate, int totalDay) throws ParseException, IOException{
-        // create an empty array
-        Data[] dataList = new Data[totalDay];
-            // read file
-        BufferedReader reader = new BufferedReader(new FileReader("data.csv"));
-        reader.readLine(); // skip 1st line
-        String line = reader.readLine(); // read each line
-        // loop through file
-        int i = 0;
-        while (line != null && i < totalDay) {
-            String[] splitData = line.split(",");
-            String countryName = splitData[2];
-            String date = splitData[3];
-
-            if (countryName.equals(country) && date.equals(startDate)) {
-                dataList[i] = new Data(countryName, date);
-                i++;
-                line = reader.readLine(); // next line
+class errorHandler {
+    public static boolean checkCountry(Data date, String country) throws IOException {
+        boolean valid = false;
+        // capitalize 1st letter
+        String formatCountry = country.substring(0, 1).toUpperCase() + country.substring(1, country.length()).toLowerCase();
+         // read file
+         BufferedReader reader = new BufferedReader(new FileReader("data.csv"));
+         reader.readLine(); // skip 1st line
+         String line = reader.readLine(); // read each line
+         // loop through file
+         while (line != null) {
+             String[] splitData = line.split(",");
+             if (splitData[2].equals(formatCountry)) {
+                 date.country = formatCountry;
+                 valid = true;
+                 break;
+             } else {
+                 line = reader.readLine();
                  continue;
-            } else if (countryName.equals(country) && dataList[0] != null) {
-                dataList[i] = new Data(countryName, date);
-                i++;
-                line = reader.readLine(); // next line
-                continue;
-            } else {
-                line = reader.readLine(); // next line
-                continue;
-            }
-        }
-        reader.close();
-        // print
-        for (int x = 0; x < dataList.length; x++) {
-            System.out.printf("\nCountry: %s, Date: %s", dataList[x].country, dataList[x].date);
-        }
-        return dataList;
+             }
+         }
+         reader.close();
+         return valid;
     }
 }
 
 class Data {
     String country;
-    String date;
+    String startDate;
+    String endDate;
+    
+    Data() {
+
+    }
 
     // create a constructor
-    Data(String country, String date) {
+    Data(String country, String startDate, String endDate) {
         this.country = country;
-        this.date = date;
+        this.startDate = startDate;
+        this.endDate = endDate;
     }
 }
