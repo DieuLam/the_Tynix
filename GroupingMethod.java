e5ff718651c7f43e4dd077974da55692bc37272f
minhtorang <s3878058@rmit.edu.vn>
2021-08-19 11:59:09 +0700

    add comment and fix bugs

--- GroupingMethod.java
+++ GroupingMethod.java
@@ -3,8 +3,8 @@ import java.text.*;
 import java.util.*;
 
 class GroupingOption {
-
     public static void groupByNumGroups(Data data) throws IOException, ParseException {
+        // get number of groups from user
         System.out.println("\nEnter number of groups");
         int numGroup = Integer.parseInt(Main.input.nextLine());
         data.DataGroups = new Group[numGroup];
@@ -12,10 +12,11 @@ class GroupingOption {
     }
 
     public static void groupByNumDays(Data data) throws IOException, ParseException {
-        System.out.println("\nEnter number of days");
-        int numDay = Integer.parseInt(Main.input.nextLine());
+        // System.out.println("\nEnter number of days");
+        // int numDay = Integer.parseInt(Main.input.nextLine());
     }
 
+    // add all selected dates to a list
     public static ArrayList<String> getTotalDays(Data obj) throws IOException, ParseException {
         ArrayList<String> dateList = new ArrayList<String>();
         BufferedReader reader = new BufferedReader(new FileReader("data.csv"));
@@ -24,13 +25,19 @@ class GroupingOption {
 
         while (line != null) {
             String[] splitData = line.split(",");
+            // if start date is found on the data.csv
             if (splitData[3].equals(obj.startDate)) {
+                // loop to add all dates from start date to before end date
                 while (true) {
+                    // add date to the list
                     dateList.add(splitData[3]);
                     line = reader.readLine();
                     splitData = line.split(",");
+                    // if end date is found on the data.csv
                     if (splitData[3].equals(obj.endDate)) {
+                        // add end date to the list
                         dateList.add(splitData[3]);
+                        // escape the loop to stop adding
                         break;
                     }
                 }
@@ -42,28 +49,35 @@ class GroupingOption {
         reader.close();
         return dateList;
     }
-}
 
-class GroupingMethods {
-    public static void groupingMethod_1() {
+    class GroupingMethods {
+        public static void groupingMethod_1() {
 
-    }
-
-    public static void groupingMethod_2(ArrayList<String> list, int numGroup, int idx, Group[] groups)
-            throws IOException, ParseException {
-
-        int totalDays = list.size();
-        int numDaysInGroup = totalDays / numGroup;
-        String[] groupList = new String[numDaysInGroup];
-
-        for (int i = 0; i < numDaysInGroup; i++) {
-            groupList[i] = (list.get(0));
-            list.remove(0);
         }
 
-        groups[idx] = new Group(groupList);
-        while (!list.isEmpty()) {
-            groupingMethod_2(list, numGroup - 1, idx + 1, groups);
+        // divde the dates into numbers of groups
+        public static void groupingMethod_2(ArrayList<String> list, int numGroup, int idx, Group[] groups)
+                throws IOException, ParseException {
+
+            int totalDays = list.size();
+            int numDaysInGroup = totalDays / numGroup;
+            // temporary group to store the dates in one group
+            String[] groupList = new String[numDaysInGroup];
+
+            // loop number-of-day times to add dates to group
+            for (int i = 0; i < numDaysInGroup; i++) {
+                // add dates from list to temp group
+                groupList[i] = (list.get(0));
+                // when a new date is added to the temp group, remove that date from the list
+                list.remove(0);
+            }
+
+            // add new group to group list of the data
+            groups[idx] = new Group(groupList);
+            // repeat until there are no dates left
+            while (!list.isEmpty()) {
+                groupingMethod_2(list, numGroup - 1, idx + 1, groups);
+            }
         }
     }
 }
