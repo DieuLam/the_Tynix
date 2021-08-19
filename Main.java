483221c864b2689196f4a23ae7354d99bf7cd541
DieuLam <s3877664@rmit.edu.vn>
2021-08-19 13:12:20 +0700

    change if else to switch case in main

--- Main.java
+++ Main.java
@@ -14,40 +14,49 @@ public class Main {
         // if country is valid
         if (errorHandler.checkCountry(date, country) == true) {
 
+            System.out.println("------------------------------------------------------");
+
             // DATA
-            System.out.println(
-                    "\n1: Start date & end date \n2: Number of days from a date \n3: Number of weeks from a date\n");
+            System.out.println("\n1: Start date & end date \n2: Number of days from a date \n3: Number of weeks from a date\n");
             System.out.print("Please choose a time range method (1/2/3): ");
             int option = Integer.parseInt(input.nextLine());
-            // option 1
-            if (option == 1) {
-                dateOption.option_1(date.country, date); // call
-                // option 2
-            } else if (option == 2) {
-                dateOption.option_2(date.country, date); // call
-                // option 3
-            } else if (option == 3) {
-                dateOption.option_3(date.country, date); // call
-            } else {
-                System.out.println("invalid option");
+            // get option
+            switch (option) {
+                case 1:
+                    dateOption.option_1(date.country, date); 
+                    break;
+                case 2:
+                    dateOption.option_2(date.country, date); 
+                    break;
+                case 3:
+                    dateOption.option_3(date.country, date); 
+                    break;
+                default:
+                    System.out.println("invalid option");
             }
+
+            System.out.println("------------------------------------------------------");
 
             // SUMMARY 2.1
-            System.out.println("\n1: No grouping \n2: Number of groups \n3: Number ofdays\n");
+            System.out.println("\n1: No grouping \n2: Number of groups \n3: Number of days\n");
             System.out.print("Please choose a grouping method (1/2/3): ");
             int groupingMethod = Integer.parseInt(input.nextLine());
-            // option 1
-            if (groupingMethod == 1) {
-
-            // option 2
-            } else if (groupingMethod == 2) {
-                GroupingOption.groupByNumGroups(date); // call
-            // option 3
-            } else if (groupingMethod == 3) {
-
-            } else {
-                System.out.println("invalid option");
+            // get option
+            switch (groupingMethod) {
+                case 1:
+                    // call method
+                    break;
+                case 2:
+                    GroupingOption.groupByNumGroups(date);
+                    break;
+                case 3:
+                    // call method
+                    break;
+                default:
+                    System.out.println("invalid option");
             }
+
+            System.out.println("------------------------------------------------------");
 
             // SUMMARY 2.2
             // THUAN put your code here
@@ -55,6 +64,7 @@ public class Main {
         } else {
             System.out.println("\nWe can't find the country you are looking for");
         }
+
         System.out.println(date);
         // System.out.println(.dateList);
         input.close();
