97f71debbf545553c11b50dd61df8bad08b898eb
minhtorang <s3878058@rmit.edu.vn>
2021-08-19 11:46:47 +0700

    implement group by number of groups

--- GroupingMethod.java
+++ GroupingMethod.java
@@ -2,63 +2,18 @@ import java.io.*;
 import java.text.*;
 import java.util.*;
 
-class GroupingMethod {
-    public static void groupingMethod_2(Data data) throws IOException, ParseException {
-        // get number of groups from users
+class GroupingOption {
+
+    public static void groupByNumGroups(Data data) throws IOException, ParseException {
         System.out.println("\nEnter number of groups");
         int numGroup = Integer.parseInt(Main.input.nextLine());
-
         data.DataGroups = new Group[numGroup];
+        GroupingMethods.groupingMethod_2(getTotalDays(data), numGroup, 0, data.DataGroups);
+    }
 
-        ArrayList<String> dateList = getTotalDays(data);
-        int totalDays = dateList.size();
-        String[] groupingList = new String[totalDays / numGroup];
-        int count = 0;
-
-        // if number of days in groups are equal
-        if (totalDays % numGroup == 0) {
-            // loop through numbers of groups
-            for (int j = 0; j < numGroup; j++) {
-                // loop through numbers of days in one group
-                for (int i = 0; i < totalDays / numGroup; i++) {
-                    // append days to the temporary group list
-                    groupingList[i] = (dateList.get(count));
-                    count++;
-                }
-                // create new array with the same value with the temporary group list
-                String[] finalGroupingList = new String[groupingList.length];
-                for (int x = 0; x < groupingList.length; x++) {
-                    finalGroupingList[x] = groupingList[x];
-                }
-                // append new group object to the group list of data
-                data.DataGroups[j] = new Group(finalGroupingList);
-                
-            }
-            System.out.println(Arrays.toString(data.DataGroups));
-        }
-
-        // if (totalDays / numGroup >= 2) {
-        //     for (int j = 0; j < numGroup - 1; j++) {
-        //         for (int i = 0; i < totalDays / numGroup; i++) {
-        //             groupingList[i] = (dateList.get(count));
-        //             count++;
-        //         }
-        //         String[] finalGroupingList = new String[groupingList.length];
-        //         finalGroupingList = groupingList;
-        //         data.DataGroups[j] = new Group(finalGroupingList);
-        //     }
-        //     for (int i = 0; i < totalDays % numGroup; i++) {
-        //         groupingList[i] = (dateList.get(count));
-        //         count++;
-        //     }
-        //     String[] finalGroupingList = new String[groupingList.length];
-        //     finalGroupingList = groupingList;
-        //     data.DataGroups[data.DataGroups.length - 1] = new Group(finalGroupingList);
-        // } 
-        else {
-            System.out.println("error");
-        }
-        
+    public static void groupByNumDays(Data data) throws IOException, ParseException {
+        System.out.println("\nEnter number of days");
+        int numDay = Integer.parseInt(Main.input.nextLine());
     }
 
     public static ArrayList<String> getTotalDays(Data obj) throws IOException, ParseException {
@@ -86,5 +41,29 @@ class GroupingMethod {
         }
         reader.close();
         return dateList;
+    }
+}
+
+class GroupingMethods {
+    public static void groupingMethod_1() {
+
+    }
+
+    public static void groupingMethod_2(ArrayList<String> list, int numGroup, int idx, Group[] groups)
+            throws IOException, ParseException {
+
+        int totalDays = list.size();
+        int numDaysInGroup = totalDays / numGroup;
+        String[] groupList = new String[numDaysInGroup];
+
+        for (int i = 0; i < numDaysInGroup; i++) {
+            groupList[i] = (list.get(0));
+            list.remove(0);
+        }
+
+        groups[idx] = new Group(groupList);
+        while (!list.isEmpty()) {
+            groupingMethod_2(list, numGroup - 1, idx + 1, groups);
+        }
     }
 }
