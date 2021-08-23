850f7bacefe55e0df8f05acff4eeb07e33a02f4d
Thuanlengoc2511 <s3877961@rmit.edu.vn>
2021-08-23 11:16:02 +0700

    push metric new total

--- Metric.java
+++ Metric.java
@@ -2,25 +2,64 @@ import java.io.*;
 import java.text.*;
 import java.util.*;
 
-
-
 class MetricOption {
-    public static void PositiveCases(Data Pcases) throws IOException, ParseException {
-        ArrayList<String[]> casenum = GroupingOption.getTotalDays(Pcases);
-        for (int i = 0; i < Pcases.DataGroups.length; i++) {
-            
-            for (int j = 0; j < Pcases.DataGroups[i].totalDays.length; j++) {
-                String new_total;
-                new_total = casenum.get(0)[1];
-                if (Pcases.DataGroups[i].value == 0) {
-                    Pcases.DataGroups[i].value = Integer.parseInt(new_total);
+    // new total
+    // calculate both new cases and death cases new total
+    public static void CasesNewTotal(Data cases, int metric) throws IOException, ParseException {
+        ArrayList<String[]> casenum = GroupingOption.getTotalDays(cases);
+        //loop number of groups
+        for (int i = 0; i < cases.DataGroups.length; i++) {
+            //loop the data in group
+            for (int j = 0; j < cases.DataGroups[i].totalDays.length; j++) {
+                int new_total;
+                // check if data is null or not
+                if (casenum.get(0)[metric].equals("")) {
+                    new_total = 0;    
+                }
+                else {
+                    new_total = Integer.parseInt(casenum.get(0)[metric]);
+                }
+                // check the data and asign new value
+                if (cases.DataGroups[i].value == 0) {
+                    cases.DataGroups[i].value = new_total;
                     casenum.remove(0);
-                }else {
-                    Pcases.DataGroups[i].value += Integer.parseInt(new_total);
+                    
+                } else {
+                    cases.DataGroups[i].value += new_total;
                     casenum.remove(0);
-                }  
+                }    
             }
-            System.out.println(Pcases.DataGroups[i].value);  
         }
     }
+
+    // calculate vaccinated new total
+    public static void VaccineNew(Data Vcases) throws IOException, ParseException {
+        ArrayList<String[]> casenum = GroupingOption.getTotalDays(Vcases);
+        //loop the number of group
+        for (int i = 0; i < Vcases.DataGroups.length; i++) {
+            int Fvaccine;
+            int Lvaccine;
+            // check the data if it null
+            if (casenum.get(0)[3].equals("")) {
+                Fvaccine = 0;
+            } else {
+                Fvaccine = Integer.parseInt(casenum.get(0)[3]);
+            }
+            if (casenum.get(Vcases.DataGroups[i].totalDays.length - 1)[3].equals("")) {
+                Lvaccine = 0;
+            } else {
+                Lvaccine = Integer.parseInt(casenum.get(Vcases.DataGroups[i].totalDays.length - 1)[3]);
+            }
+            //get the vaccinated new total
+            Vcases.DataGroups[i].value = Lvaccine - Fvaccine;
+            for (int j = 0; j < Vcases.DataGroups[i].totalDays.length; j++) {
+                casenum.remove(0);
+            }
+            System.out.println(Vcases.DataGroups[i].value);
+        }
+    }
+
+
+    // calculate Up to
+
 }
