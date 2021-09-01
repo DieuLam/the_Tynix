a448cbd0df38e76a7cdc707efbda66b3739340f1
minhtorang <s3878058@rmit.edu.vn>
2021-09-01 14:34:26 +0700

    add comment

--- Metric.java
+++ Metric.java
@@ -63,42 +63,50 @@ class MetricOption {
         ArrayList<String[]> caseNum = GetTotalDates.getTotalDays(Vcases);
         String[] day = new String[4];
 
+        // loop to find the date before the start date
         for (int i = 0; i < fdate.size(); i++) {
+            // check if we found start date and if there is a date before it
             if (fdate.get(i)[0].equals(caseNum.get(0)[0]) && !fdate.get(0)[0].equals(caseNum.get(0)[0])) {
+                // assign the data of the date before start date to a variable
                 day[3] = fdate.get(i - 1)[3];
                 day[0] = fdate.get(i - 1)[0];
                 break;
             } else {
+                // assign "0" to the vaccinated column
                 day[0] = caseNum.get(0)[0];
                 day[3] = "0";
             }
         }
+        // add the date before start date to the first position of the list
         caseNum.add(0, day);
 
+        // loop through all the dates selected by users
         for (int i = 0; i < dateList.size(); i++) {
+            // loop through all dates of the selected country
             for (int j = 0; j < fdate.size(); j++) {
-                int vaccine;
-
-                if (fdate.get(j)[3].equals("")) {
-                    vaccine = 0;
-                } else {
-                    vaccine = Integer.parseInt(fdate.get(j)[3]);
-                }
+                // get the vaccinated value of all dates in that country
+                int vaccine = Integer.parseInt(fdate.get(j)[3]);
+                // check if the vaccinated value of the first date in the list is larger than
+                // vaccinated value of all dates in that country (exclude the date before start date)
                 if (Integer.parseInt(caseNum.get(1)[3]) > vaccine) {
+                    // keep the current vaccinated value
                     caseNum.get(1)[3] = caseNum.get(1)[3];
                 } else {
+                    // assign the new vaccinated value to the first date 
                     caseNum.get(1)[3] = Integer.toString(vaccine);
                 }
+                // check if we reach the first date of the list
                 if (fdate.get(j)[0].equals(dateList.get(i)[0])) {
                     break;
                 }
             }
             int firstVaccine = Integer.parseInt(caseNum.get(0)[3]);
             int lastVaccine = Integer.parseInt(caseNum.get(1)[3]);
+            // calculate the number of people get vaccinated on that date
             dateList.get(i)[3] = Integer.toString(lastVaccine - firstVaccine);
+            // remove the first date of the group
             caseNum.remove(0);
         }
-
         return dateList;
     }
 }
